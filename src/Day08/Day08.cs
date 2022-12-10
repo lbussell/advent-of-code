609d368346e47502e9410e25788ce9d42f509b8f
Logan Bussell <loganbussell@microsoft.com>
2022-12-10 12:52:25 -0800

    Add template and day 8 infra

---
+++ src/Day08/Day08.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode;
+
+public class Day08Solution : Solution
+{
+    public Day08Solution() : base(2022, 8, "Treetop Tree House") { }
+
+    public override string SolvePartOne()
+    {
+        return "unsolved";
+    }
+
+    public override string SolvePartTwo()
+    {
+        return "unsolved";
+    }
+}
---
+++ src/Template/DayXX.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode;
+
+public class DayXXSolution : Solution
+{
+    public DayXXSolution() : base(2022, 0, "Title here") { }
+
+    public override string SolvePartOne()
+    {
+        return "unsolved";
+    }
+
+    public override string SolvePartTwo()
+    {
+        return "unsolved";
+    }
+}
