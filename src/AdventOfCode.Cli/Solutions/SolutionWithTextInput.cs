717a1e697ca445a1f6550d2db6b70960b84d02f2
Logan Bussell false <loganbussell@microsoft.com>
2023-11-30 10:05:43 -0800

    Fix input for benchmarks

--- src/AdventOfCode.Cli/Solutions/SolutionWithTextInput.cs
+++ src/AdventOfCode.Cli/Solutions/SolutionWithTextInput.cs
@@ -1,21 +1,27 @@
 namespace Bussell.AdventOfCode.Solutions;
 
+using System.Runtime.CompilerServices;
+
 public abstract class SolutionWithTextInput : ISolution
 {
     public SolutionWithTextInput()
     {
-        Input = File.ReadAllLines(InputFileName);
+        string basePath = Path.GetDirectoryName(WhereAmI())
+            ?? throw new InvalidOperationException("Could not determine base path.");
+        Input = File.ReadAllLines(Path.Combine(basePath, InputFileName));
     }
 
     public abstract int Day { get; }
 
     public abstract string Name { get; }
 
-    protected string InputFileName => $"Inputs/{Day:D2}.txt";
+    protected string InputFileName => $"../Inputs/{Day:D2}.txt";
 
     protected IEnumerable<string> Input { get; }
 
     public abstract string SolvePart1();
 
     public abstract string SolvePart2();
+
+    private static string WhereAmI([CallerFilePath] string callerFilePath = "") => callerFilePath;
 }
