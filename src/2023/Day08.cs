acb5ad73828903378e768bd09408d235df571679
Logan Bussell <loganbussell@microsoft.com>
2023-12-22 09:34:34 -0500

    Finish day 8 part 2

--- src/2023/Day08.cs
+++ src/2023/Day08.cs
@@ -8,6 +8,10 @@ using Node = (string L, string R);
 
 public sealed class Day08() : ISolution
 {
+    private Graph _graph = [];
+
+    private bool[] _sequence = [];
+
     public int Year => 2023;
 
     public int Day => 8;
@@ -19,24 +23,24 @@ public sealed class Day08() : ISolution
         SolvePart2
     ];
 
-    private static string SolvePart1(IEnumerable<string> input)
+    private string SolvePart1(IEnumerable<string> input)
     {
         // L = false, R = right
-        bool[] sequence = GetSequence(input.First());
-        Graph graph = GetGraph(input.Skip(2));
+        _sequence = GetSequence(input.First());
+        _graph = GetGraph(input.Skip(2));
 
         string current = "AAA";
         int sequencePtr = 0;
         int steps = 0;
         while (current != "ZZZ")
         {
-            if (!graph.TryGetValue(current, out Node n))
+            if (!_graph.TryGetValue(current, out Node n))
             {
                 throw new Exception();
             }
 
-            current = sequence[sequencePtr] ? n.R : n.L;
-            sequencePtr = (sequencePtr + 1) % sequence.Length;
+            current = _sequence[sequencePtr] ? n.R : n.L;
+            sequencePtr = (sequencePtr + 1) % _sequence.Length;
             steps += 1;
         }
 
@@ -66,5 +70,36 @@ public sealed class Day08() : ISolution
     private static bool[] GetSequence(string input)
         => input.ToCharArray().Select(c => c == 'R').ToArray();
 
-    private static string SolvePart2(IEnumerable<string> input) => throw new NotImplementedException();
+    private string SolvePart2(IEnumerable<string> input)
+    {
+        _sequence = GetSequence(input.First());
+        _graph = GetGraph(input.Skip(2));
+
+        // Make some silly assumptions I read on reddit after I got stuck
+        // - The answer is the LCM of each cycle, and the cycle length is
+        //   equal to the number of steps from each A to the first Z.
+        return _graph.Keys
+            .Where(s => s[2] == 'A')
+            .Select(StepsToZ)
+            .LCM()
+            .ToString();
+    }
+
+    private long StepsToZ(string s)
+    {
+        long steps = 0;
+        long sequencePtr = 0;
+        string current = s;
+        while (current[2] != 'Z')
+        {
+            _graph.TryGetValue(current, out Node node);
+            current = _sequence[sequencePtr] ? node.R : node.L;
+            sequencePtr = (sequencePtr + 1) % _sequence.Length;
+            steps += 1;
+        }
+
+        long cycleLength = steps;
+        Console.WriteLine(cycleLength);
+        return cycleLength;
+    }
 }
--- src/AdventOfCode.Core/ExtensionMethods.cs
+++ src/AdventOfCode.Core/ExtensionMethods.cs
@@ -6,6 +6,10 @@ public static class ExtensionMethods
 {
     public static int Product(this IEnumerable<int> source) => Product<int, int>(source);
 
+    public static int LCM(this IEnumerable<int> source) => source.Aggregate(LCM);
+
+    public static long LCM(this IEnumerable<long> source) => source.Aggregate(LCM);
+
     private static TResult Product<TSource, TResult>(this IEnumerable<TSource> source)
         where TSource : struct, INumber<TSource>
         where TResult : struct, INumber<TResult>
@@ -21,4 +25,12 @@ public static class ExtensionMethods
 
         return product;
     }
+
+    private static int LCM(int a, int b) => a / GCD(a, b) * b;
+
+    private static int GCD(int a, int b) => b == 0 ? a : GCD(b, a % b);
+
+    private static long LCM(long a, long b) => a / GCD(a, b) * b;
+
+    private static long GCD(long a, long b) => b == 0 ? a : GCD(b, a % b);
 }
