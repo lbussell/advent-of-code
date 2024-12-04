bbf7dd7474b6cffefec8e10530a49c4881282ab7
Logan Bussell <loganbussell@microsoft.com>
2024-12-04 07:34:25 -0800

    Add new test

--- src/AdventOfCode.Tests/Tests.fs
+++ src/AdventOfCode.Tests/Tests.fs
@@ -15,6 +15,7 @@ module Tests =
     [<InlineData(2, 1, "2a.txt", 4)>]
     [<InlineData(2, 1, "2b.txt", 10)>]
     [<InlineData(2, 1, "2c.txt", 0)>]
+    [<InlineData(2, 1, "2d.txt", 1)>]
     let ``Test day`` day part inputFile expectedOutput =
         let input = inputFile |> File.ReadAllLines
         let solver = Solutions.getSolver day part
