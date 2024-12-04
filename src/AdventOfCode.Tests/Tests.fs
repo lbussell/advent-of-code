bc1edfdb9093d2552e7a6c3419157aff2cb77f05
Logan Bussell <loganbussell@microsoft.com>
2024-12-04 06:55:33 -0800

    Finish day 2

--- src/AdventOfCode.Tests/Tests.fs
+++ src/AdventOfCode.Tests/Tests.fs
@@ -1,6 +1,5 @@
 namespace AdventOfCode.Tests
 
-open System
 open System.IO
 open FluentAssertions
 open Xunit
@@ -12,14 +11,12 @@ module Tests =
     [<Theory>]
     [<InlineData(1, 0, "1a.txt", 11)>]
     [<InlineData(1, 1, "1a.txt", 31)>]
-    [<InlineData(2, 0, "1a.txt", 2)>]
-    [<InlineData(2, 1, "1a.txt", 0)>]
+    [<InlineData(2, 0, "2a.txt", 2)>]
+    [<InlineData(2, 1, "2a.txt", 4)>]
+    [<InlineData(2, 1, "2b.txt", 10)>]
+    [<InlineData(2, 1, "2c.txt", 0)>]
     let ``Test day`` day part inputFile expectedOutput =
         let input = inputFile |> File.ReadAllLines
         let solver = Solutions.getSolver day part
         let result = solver input
         result.Should().Be(expectedOutput)
-
-    // [<Fact>]
-    // let ``My test`` () =
-    //     Assert.True(true)
