f9a677a64a8335f22d2ba5f82fda404eac526622
Logan Bussell <loganbussell@microsoft.com>
2025-11-27 09:33:05 -0800

    Add file header

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// SPDX-FileCopyrightText: Copyright (c) 2025 Logan Bussell
+// SPDX-License-Identifier: MIT
+
 Console.WriteLine("Hello, World!");
